d40d2303a6faddc5eb7427d1e2256451fd47a7e4
Dylan TAILDEMAN <taildeman_dylan@outlook.com>
2022-04-05 09:38:35 +0200

    Created a template file for new commands

---
+++ template.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+# Importing bot modules
+from log import log, not_alone
+
+# Commands
+@commands.command(name="", help="")
+async def name(ctx):
+    # Command code here
+    await ctx.send("Hello World!")
+
+# Code
+def setup(bot):
+    bot.add_command(name)
+
+log(f"{__name__} module loaded!")
+
+# Block execution of this file alone
+if __name__ == "__main__":
+    not_alone()
